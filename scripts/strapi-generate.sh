e1b7e525b8d3d2d46efa39485007ba3f5b9a1615
Bobae Kang <bkang.dev@gmail.com>
2019-08-30 11:49:24 -0500

    feat: update strapi api fields

--- scripts/strapi-generate.sh
+++ scripts/strapi-generate.sh
@@ -7,32 +7,29 @@ generate_api() {
     fi
 }
 
+# common attrs
+ATTRS_COMMON="status:string title:string slug:string date:date external:boolean"
+ATTRS_COMMON="$ATTRS_COMMON categories:json tags:json"
+
 ## generate app type
 NAME="app"
-ATTRS="title:string published:boolean external:boolean slug:string date:date"
-ATTRS="$ATTRS categories:json tags:json image:string contributors:json"
-ATTRS="$ATTRS description:text src:string url:string citation:string"
-ATTRS="$ATTRS funding:string"
+ATTRS="$ATTRS_COMMON"
+ATTRS="$ATTRS contributors:json image:string description:text url:string"
+ATTRS="$ATTRS funding:string citation:string"
 generate_api
 
 ## generate article type
 NAME="article"
-ATTRS="title:string published:boolean external:boolean slug:string date:date"
-ATTRS="$ATTRS type:json categories:json tags:json splash:string"
-ATTRS="$ATTRS thumbnail:string images:json abstract:string markdown:text"
-ATTRS="$ATTRS citation:string doi:string funding:string"
-generate_api
-
-## generate author type
-NAME="author"
-ATTRS="title:string external:boolean slug:string description:text"
+ATTRS="$ATTRS_COMMON"
+ATTRS="$ATTRS authors:json splash:string thumbnail:string images:json"
+ATTRS="$ATTRS abstract:string markdown:text"
+ATTRS="$ATTRS mainfiletype:string funding:string citation:string doi:string"
 generate_api
 
 ## generate dataset type
 NAME="dataset"
-ATTRS="title:string published:boolean external:boolean slug:string date:date"
-ATTRS="$ATTRS categories:json tags:json sources:json unit:string"
-ATTRS="$ATTRS agegroup:string timeperiod:json description:text notes:json"
-ATTRS="$ATTRS variables:json citation:string funding:string datacsv:json"
-ATTRS="$ATTRS  datafilename:string"
+ATTRS="$ATTRS_COMMON"
+ATTRS="$ATTRS project:boolean sources:json unit:string timeperiod:json"
+ATTRS="$ATTRS description:text notes:json variables:json"
+ATTRS="$ATTRS funding:string citation:string"
 generate_api
